3763fc6809905d706e081540f1d0e89b80f968df
Teddygat0r <zhy0224@gmail.com>
2022-07-10 22:22:57 -0700

    stuff

---
+++ js/index.js
@@ -0,0 +1,125 @@
+import * as THREE from 'three';
+import { BoxGeometry, Raycaster } from 'three';
+import { OrbitControls } from 'https://unpkg.com/three@0.142.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.142.0/examples/jsm/loaders/GLTFLoader.js';
+
+
+const detectWebGLContext = () => {
+    // Create canvas element. The canvas is not added to the
+    // document itself, so it is never displayed in the
+    // browser window.
+    var canvas = document.createElement("canvas");
+    // Get WebGLRenderingContext from canvas element.
+    var gl =
+        canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    // Report the result.
+    if (gl && gl instanceof WebGLRenderingContext) {
+        return true;
+    } else {
+        alert("You don't have WebGL enabled :(");
+        return false;
+    }
+}
+
+
+
+function main() {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({ canvas });
+
+    const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+    camera.position.set(0, 0, 8);
+    camera.lookAt(0, 0, 0);
+    let renderRequested = false;
+
+    const scene = new THREE.Scene();
+
+    const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 0, 0);
+    controls.update();
+    controls.enableDamping = true;
+
+
+    //drawing
+    const cube = makeInstance(new BoxGeometry(8, 2, 0.95), 0xff0000, 0, 0.25, -0.07);
+
+    const loader = new GLTFLoader();
+    /**loader.load('./models/words.glb', (gltf)=>{
+        scene.add(gltf.scene);
+        requestRenderIfNotRequested();
+    }, undefined, (error)=>{
+        console.log(error);
+    }); */
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+
+        const light2 = new THREE.DirectionalLight(color, intensity);
+        light2.position.set(1, -3, -2);
+        scene.add(light2);
+    }
+
+    //RayCasting
+    const raycaster = new THREE.Raycaster();
+    const pointer = new THREE.Vector2();
+    function onPointerMove(event){
+        pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	    pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    }
+
+
+    //Resizes the Display if needed.
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const pixelRatio = window.devicePixelRatio;
+        const width = canvas.clientWidth * pixelRatio | 0;
+        const height = canvas.clientHeight * pixelRatio | 0;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+
+    //render
+    function render() {
+        renderRequested = false;
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+
+        controls.update();
+        renderer.render(scene, camera);
+    }
+
+    //Makes it so only one render is called at a time.
+    function requestRenderIfNotRequested() {
+        if (!renderRequested) {
+            renderRequested = true;
+            requestAnimationFrame(render);
+        }
+    }
+    render();
+
+    //Function that simplifies adding a shape to the scene.
+    function makeInstance(geometry, color, x, y, z) {
+        const material = new THREE.MeshPhongMaterial({ color });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        cube.position.x = x;
+        cube.position.y = y;
+        cube.position.z = z;
+        return cube;
+    }
+
+}
+
+
+main(); 
